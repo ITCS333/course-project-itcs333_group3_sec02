8ca44d5f4096c41e8d68e260d4343744f1ae13e2
wexdan3 <wejdanahmed610@gmail.com>
2025-12-06 23:36:42 +0300

    Refactor Database class to use private properties

--- src/config/Database.php
+++ src/config/Database.php
@@ -1,22 +1,28 @@
 <?php
 class Database {
-    $host = 'localhost';
-    $db   = 'course';
-    $user = 'admin';
-    $pass = 'password123';
+
+    private $host = 'localhost';
+    private $db   = 'course';
+    private $user = 'admin';
+    private $pass = 'password123';
+
     public $pdo;
 
     public function getConnection() {
-        $this-> pdo = null;
+        $this->pdo = null;
+
         try {
-            $dsn = "mysql:host=" .$this->host.";dbname=".$this -> db;
-            $pdo = new PDO($dsn, $this->user, $this->pass);
-            $pdo-> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
-        } catch (\PDOException $e) {
-            throw new \PDOException($e->getMessage(), (int)$e->getCode());
+            $dsn = "mysql:host={$this->host};dbname={$this->db};charset=utf8mb4";
+
+            $this->pdo = new PDO($dsn, $this->user, $this->pass);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+        } catch (PDOException $e) {
+            throw new PDOException($e->getMessage(), (int)$e->getCode());
         }
-        return $pdo;
+
+        return $this->pdo;
     }
 }
 ?>
